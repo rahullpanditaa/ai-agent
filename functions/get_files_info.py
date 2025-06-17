56fa8d19acf2008514a68331d31c183c3494f440
Rahul Pandita <rpandita1999@outlook.com>
2025-06-17 09:13:47 +0000

    get file path of working_directory and directory to search in

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,6 +4,11 @@ from pathlib import Path
 def get_files_info(working_directory, directory=None):
     work_dir_path = os.path.abspath(working_directory)
     search_in_dir = os.path.abspath(directory)
-    print(search_in_dir.startswith(work_dir_path))
+    if not search_in_dir.startswith(work_dir_path):
+        return f"Error: Cannot list {directory} as it is outside the permitted working directory"
+    
+    if not os.path.isdir(directory):
+        return f"Error: {directory} is not a directory"
 
-get_files_info("calculator", ".")
+
+# get_files_info(".", "calculator")
