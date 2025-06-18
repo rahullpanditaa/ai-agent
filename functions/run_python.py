cdc80998203fdea70228abfe3b9b1f82e755c0c9
Rahul Pandita <rpandita1999@outlook.com>
2025-06-18 06:27:13 +0000

    run_python_file: all tests pass

--- functions/run_python.py
+++ functions/run_python.py
@@ -9,7 +9,7 @@ def run_python_file(working_directory, file_path, args=None):
         return f"Error: Cannot execute \"{file_path}\" as it is outside the permitted working directory."
     
     if not os.path.exists(target_file_path):
-        return f"Error: \"{file_path}\" not found."
+        return f"Error: File \"{file_path}\" not found."
     
     if not target_file_path.endswith(".py"):
         return f"Error: \"{file_path}\" is not a Python file."
--- tests.py
+++ tests.py
@@ -2,5 +2,8 @@ from functions.run_python import run_python_file
 
 def test_get_files_info():
     print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator","../main.py"))
+    print(run_python_file("calculator", "blah_blah.py"))
 
 test_get_files_info()
