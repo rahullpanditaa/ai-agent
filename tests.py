388c7bce19696dbd1b4272e5caf11cfcfe9f7836
Rahul Pandita <rpandita1999@outlook.com>
2025-06-18 05:45:51 +0000

    all tests pass

--- tests.py
+++ tests.py
@@ -2,5 +2,7 @@ from functions.write_file import write_file
 
 def test_get_files_info():
     print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not bew allowed"))
 
 test_get_files_info()
