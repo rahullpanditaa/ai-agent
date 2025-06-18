b154944104bdc7157075d42c02a03d62203f1ef2
Rahul Pandita <rpandita1999@outlook.com>
2025-06-18 02:58:52 +0000

    create method to get the contents of a file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -28,3 +28,17 @@ def get_files_info(working_directory, directory=None):
     except Exception:
         return "Error getting contents of the directory"
 
+def get_file_content(workin_directory, file_path):
+    work_dir_path = os.path.abspath(workin_directory)
+    target_file_path = os.path.abspath(os.path.join(workin_directory,file_path))
+
+    if not target_file_path.startswith(work_dir_path):
+        return f"Error: Cannot read {file_path} as it is outside the permitted working directory"
+    
+    if not os.path.isfile(target_file_path):
+        return f"Error: File not found or is not a regular file: {file_path}"
+    
+    with open(target_file_path) as file:
+        file_contents = file.read(1000)
+        if len(file_contents) > 10000:
+            file_contents += f"[...File \"{file_path}\" truncated at 10000 characters]"
