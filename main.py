40d6d5d6a6d75500548dbf007d74f17a1cfed6dd
Rahul Pandita <rpandita1999@outlook.com>
2025-06-19 02:23:05 +0000

    agent can now choose which function to call

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -31,7 +34,9 @@ def main():
     
     if response.function_calls:
         for func in response.function_calls:
-            print(f"Calling function : {func.name}({func.args})")
+            # print(f"Calling function : {func.name}({func.args})")
+            print(func.name)
+            print(func.args)
     else:
         print(response.text)
 
@@ -107,7 +112,7 @@ schema_write_file = types.FunctionDeclaration(
 )
 
 # functions available to the LLM
-available_functions = types.Tool(function_declarations=[schema_get_files_info])
+available_functions = types.Tool(function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file])
 
 def generate_response(conversation_history):
     return client.models.generate_content(
